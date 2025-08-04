2dc973a5eaa40f374e14455903b23f9b7e02cfbd
Ramkishan <Ramkishan@LAPTOP-TOORT93C>
2025-08-04 19:41:05 +0530

    this is my output

---
+++ com.java/src/com/java/CharDemo.java
@@ -0,0 +1,25 @@
+package com.java;
+
+public class CharDemo {
+	public static void main(String[] args) {
+		
+	
+	
+	//assigning single character literal
+	
+			char ch='a';
+			System.out.println(ch);
+			
+			//assigning  num to char
+			char ch1=65;
+			
+			System.out.println(ch1);
+			
+			int a='A';
+			
+
+		}
+
+	}
+
+
---
+++ com.java/src/com/java/Decisionmaking.java
@@ -0,0 +1,29 @@
+package com.java;
+
+public class Decisionmaking {
+	public static void main(String[] args) {
+		int a=10,b=20,c=30;
+		if (a > c) {
+			if (a > c) {
+			System.out.println("a is greater.");
+			
+		}
+			else {
+				System.out.println("c is greater.");
+			}
+		}
+			else {
+				if (c > b) {
+					System.out.println("c is greater.");
+				}
+				else {
+					System.out.println("b is greater.");
+				}
+			}
+		
+		}
+
+
+	}
+
+
