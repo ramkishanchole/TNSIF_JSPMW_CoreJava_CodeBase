0ead96a809c79f596fbdc4adad4e226abbc7e5ae
Ramkishan <Ramkishan@LAPTOP-TOORT93C>
2025-09-09 22:21:37 +0530

    oops concept

---
+++ Day03/src/com/tnsif/daythree/constructor/EncapsulationDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.daythree.constructor;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation
+
+		OopsConceptDemo obj=new OopsConceptDemo();
+		
+		obj.setAge(34);
+		obj.setName("Manoj");
+		obj.setSerialNum(12345);
+		
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+		System.out.println(obj.getSerialNum());
+		
+		System.out.println(obj);
+
+	}
+
+}
